daf980a6f40ccaf36562ad12d355741dbb286d92
Nickinfo1 <nickinfo1@yandex.ru>
2025-07-13 18:45:22 +0500

    task 1-2-2 is done

--- algo_1/1-2/1-2-2.js
+++ algo_1/1-2/1-2-2.js
@@ -18,25 +18,40 @@ function mergeSortNumberOperations(n) {
   return 64 * Math.log2(n);
 }
 
+function operation(i) {
+  const seconds_insert = insertSortNumberOperations(i) / TASKS_PER_SECOND_CPU1;
+  const seconds_merge = mergeSortNumberOperations(i) / TASKS_PER_SECOND_CPU1;
+  const diff = seconds_insert - seconds_merge;
+  // console.log(Math.floor(diff));
+  return { diff: diff, insert: seconds_insert, merge: seconds_merge };
+}
+
 function compare_algo_time(n) {
-  let time;
   const arr = [];
   for (let i = 1; i < n; i++) {
-    const seconds_insert =
-      insertSortNumberOperations(i) / TASKS_PER_SECOND_CPU1;
-    const seconds_merge = mergeSortNumberOperations(i) / TASKS_PER_SECOND_CPU2;
-    const diff = seconds_insert - seconds_merge;
-    if (diff === 0) {
-      console.log(n);
+    const { diff, insert, merge } = operation(i);
+    if (arr.length < 6) {
+      arr.push(diff);
+    } else {
+      arr.shift();
+      arr.push(diff);
     }
-    // console.log(
-    //   `Элементов: ${n} | Вставкой: ${seconds_insert} | Слиянием: ${seconds_merge} | Разность: ${diff === 0 ? "Одинаково" : diff} | ${diff > 0 ? "слиянием быстрее" : "вставкой быстрее"}`,
-    // );
+    if (Math.floor(arr[0]) > 0 && Math.floor(arr[1]) == 0) {
+      console.log(`вставкой стало быстрее ${i}`);
+      console.log(`${JSON.stringify(arr)} | n = ${i}`);
+    }
+    if (Math.floor(arr[0]) == 0 && Math.floor(arr[1]) > 0) {
+      // вставкой быстрее потом слиянием
+      console.log(`слиянием стало быстрее ${i}`);
+      console.log(`${JSON.stringify(arr)} | n = ${i}`);
+    }
+    if (i % 100_000 === 0) {
+      console.log(
+        `n = ${i}, diff=${diff}, insert = ${insert}, merge = ${merge}`,
+      );
+    }
   }
 }
 
-for (let i = 1; i < 1000000; i++) {
-  compare_algo_time(i);
-}
-
-// В итоге с ростом количества элементов Вставка работает быстрее
+const oper = 1_000_000;
+compare_algo_time(oper);
